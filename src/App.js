1bee7f31023322ce43433816a179d97d9218a12f
oduodg <werewildfire@gmail.com>
2022-09-20 14:43:23 +0900

    add cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Cards from './components/Cards';
 import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Cards/>
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+export default function Cards() {
+	return (
+		<>
+			<div className="flex mx-28 mt-7">
+
+				<div
+					id="Mon"
+					className="bg-[url('./imgs/bg1.png')] bg-contain bg-center bg-no-repeat w-56 h-80 mx-3">
+					<p className="mt-3 text-xs text-[#BABFC8]">Mon.</p>
+					<button className="font-medium mt-2 bg-[#4DC591] rounded-3xl w-44 py-1 text-sm text-[#FFFFFF]">오징어야채볶음</button>
+					<div className="menu mt-5 mb-14 text-[#FFFFFF] text-base">
+						<p>짜장면</p>
+						<p>탕수육, 소스</p>
+						<p>양배추샐러드</p>
+						<p>짬뽕국물</p>
+						<p>단무지</p>
+						<p>주스</p>
+					</div>
+					<p className="text-[#BABFC8] text-sm">917 Kal</p>
+				</div>
+
+				<div
+					id="Tue"
+					className="bg-[url('./imgs/bg2.png')] bg-contain bg-center bg-no-repeat w-56 h-80 mx-3">
+					<p className="mt-3 text-xs text-[#BABFC8]">Tue.</p>
+					<button className="font-medium mt-2 bg-[#4DC591] rounded-3xl w-44 py-1 text-sm text-[#FFFFFF]">오징어야채볶음</button>
+					<div className="menu mt-5 mb-14 text-[#FFFFFF] text-base">
+						<p>짜장면</p>
+						<p>탕수육, 소스</p>
+						<p>양배추샐러드</p>
+						<p>짬뽕국물</p>
+						<p>단무지</p>
+						<p>주스</p>
+					</div>
+					<p className="text-[#BABFC8] text-sm">917 Kal</p>
+				</div>
+
+				<div
+					id="Wed"
+					className="bg-[url('./imgs/bg3.png')] bg-contain bg-center bg-no-repeat w-56 h-80 mx-3">
+					<p className="mt-3 text-xs text-[#BABFC8]">Wed.</p>
+					<button className="font-medium mt-2 bg-[#4DC591] rounded-3xl w-44 py-1 text-sm text-[#FFFFFF]">오징어야채볶음</button>
+					<div className="menu mt-5 mb-14 text-[#FFFFFF] text-base">
+						<p>짜장면</p>
+						<p>탕수육, 소스</p>
+						<p>양배추샐러드</p>
+						<p>짬뽕국물</p>
+						<p>단무지</p>
+						<p>주스</p>
+					</div>
+					<p className="text-[#BABFC8] text-sm">917 Kal</p>
+				</div>
+
+				<div
+					id="Thu"
+					className="bg-[url('./imgs/bg4.png')] bg-contain bg-center bg-no-repeat w-56 h-80 mx-3">
+					<p className="mt-3 text-xs text-[#BABFC8]">Thu.</p>
+					<button className="font-medium mt-2 bg-[#4DC591] rounded-3xl w-44 py-1 text-sm text-[#FFFFFF]">오징어야채볶음</button>
+					<div className="menu mt-5 mb-14 text-[#FFFFFF] text-base">
+						<p>짜장면</p>
+						<p>탕수육, 소스</p>
+						<p>양배추샐러드</p>
+						<p>짬뽕국물</p>
+						<p>단무지</p>
+						<p>주스</p>
+					</div>
+					<p className="text-[#BABFC8] text-sm">917 Kal</p>
+				</div>
+
+				<div
+					id="Fri"
+					className="bg-[url('./imgs/bg5.png')] bg-contain bg-center bg-no-repeat w-56 h-80 mx-3">
+					<p className="mt-3 text-xs text-[#BABFC8]">Fri.</p>
+					<button className="font-medium mt-2 bg-[#4DC591] rounded-3xl w-44 py-1 text-sm text-[#FFFFFF]">오징어야채볶음</button>
+					<div className="menu mt-5 mb-14 text-[#FFFFFF] text-base">
+						<p>짜장면</p>
+						<p>탕수육, 소스</p>
+						<p>양배추샐러드</p>
+						<p>짬뽕국물</p>
+						<p>단무지</p>
+						<p>주스</p>
+					</div>
+					<p className="text-[#BABFC8] text-sm">917 Kal</p>
+				</div>
+			</div>
+		</>
+	)
+}
