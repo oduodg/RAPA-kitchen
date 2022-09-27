666eb5d67b7b4d8f78b25e87dbec227f7a2cb8bd
oduodg <werewildfire@gmail.com>
2022-09-27 13:39:19 +0900

    fixed proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,23 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
 	app.use(
-		'/',
+		'/menu',
+		createProxyMiddleware({
+			target: 'http://localhost:8080',
+			changeOrigin: true,
+		})
+	);
+
+	app.use(
+		'/comment',
+		createProxyMiddleware({
+			target: 'http://localhost:8080',
+			changeOrigin: true,
+		})
+	);
+
+	app.use(
+		'/wishmenu',
 		createProxyMiddleware({
 			target: 'http://localhost:8080',
 			changeOrigin: true,
